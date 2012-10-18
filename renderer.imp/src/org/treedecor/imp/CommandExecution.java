6895cd57258dac7b20d5269165f771e85bff42a8
Sebastian Erdweg <seba@informatik.uni-marburg.de>
2012-10-18 11:30:38 +0200

    drop dep on Java1.7, rm superfluous import

--- renderer.imp/src/org/treedecor/imp/CommandExecution.java
+++ renderer.imp/src/org/treedecor/imp/CommandExecution.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.lang.invoke.VolatileCallSite;
 import java.util.concurrent.CountDownLatch;
 
 /**

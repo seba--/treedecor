db44624277735be8691b8fdafda707632f579633
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2012-11-19 15:50:57 +0100

    Fix parser service test script

--- parsing-aas/test.sh
+++ parsing-aas/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -x POST -H 'Content-Type:application/x-sdf' --data-binary @../syntax.xml/xml.def http://127.0.0.1:8080/grammar?module=xml
+curl -X POST -H 'Content-Type:application/x-sdf' --data-binary @../syntax.xml/xml.def http://127.0.0.1:8080/grammar?module=xml
 
 for i in {1..100}
 do

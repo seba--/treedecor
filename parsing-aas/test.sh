11ca283bf82eac283b75eb68b8517bfdb258f758
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2012-11-22 22:45:14 +0100

    Make test script take a host parameter

--- parsing-aas/test.sh
+++ parsing-aas/test.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-curl -X POST -H 'Content-Type:application/x-sdf' --data-binary @../syntax.xml/xml.def http://127.0.0.1:8080/grammar?module=xml
+
+# Call with host:port as first argument, e.g.
+# sh test.sh vh12009.mathematik.uni-marburg.de:55123
+
+HOST=$1
+
+curl -X POST -H 'Content-Type:application/x-sdf' --data-binary @../syntax.xml/xml.def "http://$HOST/grammar?module=xml"
 
 for i in {1..100}
 do
-	curl -X GET -H 'Content-Type: application/binary' -d @../renderer.imp/plugin.xml http://127.0.0.1:8080/parse/-993726346?disableSourceLocationInformation=true
+	curl -X GET -H 'Content-Type: application/binary' -d @../renderer.imp/plugin.xml "http://$HOST/parse/-993726346?disableSourceLocationInformation=true"
 done
 
 for i in {1..100}
 do
-	curl -X GET -H 'Content-Type: application/binary' -d @../renderer.imp/plugin.xml http://127.0.0.1:8080/parse/-993726346
+	curl -X GET -H 'Content-Type: application/binary' -d @../renderer.imp/plugin.xml "http://$HOST/parse/-993726346"
 done
